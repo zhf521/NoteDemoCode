9ff36bdd57894432dcb04e188a1658c9030f4d91
18866805256 <3407085928@qq.com>
2023-06-03 20:33:59 +0800

    VueCLI

---
+++ Vue/16-VueCLI脚手架/demo01/src/components/Student.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "zhf",
+      age: 18,
+    }
+  }
+}
+</script>
